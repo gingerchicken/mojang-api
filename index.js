ffe5c887c52b4fc2d45405317fbd9e7cf03cf63d
HowITsDone <32576907+gingerchicken@users.noreply.github.com>
2021-08-11 19:15:38 +0100

    Migrated to promises

--- index.js
+++ index.js
@@ -1,1 +1,84 @@
-module.exports = require('./lib/index');
+const axios = require('axios');
+const API_BASE_URL      = 'https://api.mojang.com';
+const SESSION_BASE_URL  = 'https://sessionserver.mojang.com';
+
+class MojangAPI {
+    constructor() {
+        // TODO Something... maybe make an axios session or something...
+    }
+
+    #timestamp(time) {
+        let unixTimestamp;
+
+        if (time instanceof Date) {
+            unixTimestamp = Math.ceil(time.getTime() / 1000);
+        } else if (typeof time === 'number') {
+            unixTimestamp = Math.ceil(time);
+        } else {
+            throw new Error('timestamp was not a Number or Date');
+        }
+
+        return unixTimestamp;
+    }
+
+    async #parseResp(axiPromise) {
+        let resp = await axiPromise;
+        let body = resp.data;
+
+        if (body.error) throw new Error(`${body.error}: ${body.errorMessage}`);
+
+        return body;
+    }
+
+    async uuidAt(username, time) {
+        if (typeof username !== "string") throw new Error('username is not a string');
+
+        let unixTimestamp = this.#timestamp(time);
+
+        return await this.#parseResp(
+            axios({
+                url: `${API_BASE_URL}/users/profiles/minecraft/${encodeURIComponent(username)}?at=${unixTimestamp}`,
+                method: 'get'
+            })
+        );
+    }
+
+    async nameHistory(uuid) {
+        if (typeof uuid !== "string") throw new Error('uuid is not a string');
+
+        return await this.#parseResp(
+            axios({
+                url: `${API_BASE_URL}/user/profiles/${encodeURIComponent(uuid)}/names`,
+                method: 'get'
+            })
+        );
+    }
+
+    async nameToUuid(names) {
+        let nameArray;
+        if (names instanceof Array) nameArray = names;
+        else if (typeof names === "string") nameArray = [names];
+        else throw new Error('names is not a string or Array');
+
+        return await this.#parseResp(
+            axios({
+                url: `${API_BASE_URL}/profiles/minecraft`,
+                method: 'post',
+                data: nameArray
+            })
+        );
+    }
+
+    async profile(uuid) {
+        if (typeof uuid !== "string") throw new Error('uuid is not a string');
+
+        return await this.#parseResp(
+            axios({
+                url: `${SESSION_BASE_URL}/session/minecraft/profile/${encodeURIComponent(uuid)}`,
+                method: 'get'
+            })
+        );
+    }
+}
+
+module.exports = MojangAPI;
---
+++ test/index.js
@@ -0,0 +1,144 @@
+let MojangAPI = require('../index');
+
+let chai = require('chai');
+
+chai.use( require('chai-as-promised') );
+
+// This username and UUID is unlikely to change, given this is one of the "official" Mojang skin accounts.
+// See http://minecraft.gamepedia.com/Mob_head#Mojang_skins
+const USERNAME = 'MHF_Chicken';
+const UUID = '92deafa9430742d9b00388601598d6c0';
+
+// For namesToUuid test, second username:
+const USERNAME2 = 'MHF_Cake';
+
+describe('Mojang API', () => {
+    let api;
+    beforeEach(() => {
+        api = new MojangAPI();
+    });
+
+    describe('uuidAt', () => {
+        describe('throws an Error if', () => {
+            async function testReject(...args) {
+                await chai.expect(api.uuidAt(...args)).be.rejectedWith(Error);
+            }
+
+            it('username was not specified', async () => {
+                await testReject();
+            });
+
+            it('username was not valid', async () => {
+                await testReject(42);
+            });
+
+            it('date was not specified', async () => {
+                await testReject(USERNAME);
+            });
+
+            it('date was not valid', async () => {
+                await testReject(USERNAME, 'derp');
+            });
+        });
+
+        describe('works when timestamp is a', () => {
+            const TIMESTAMP = 1432789369;
+
+            it('number', async () => {
+                let resp = await api.uuidAt(USERNAME, TIMESTAMP);
+
+                chai.expect(resp).to.have.property('id').and.equal(UUID);
+                chai.expect(resp).to.have.property('name', USERNAME);
+            });
+
+            it('Date', async () => {
+                let resp = await api.uuidAt(USERNAME, new Date(TIMESTAMP * 1000));
+                    
+                chai.expect(resp).to.have.property('id', UUID);
+                chai.expect(resp).to.have.property('name', USERNAME);
+            });
+        });
+    });
+
+    describe('nameHistory', () => {
+        describe('throws an Error if', () => {
+            async function testReject(...args) {
+                await chai.expect(api.nameHistory(...args)).be.rejectedWith(Error);
+            }
+
+            it('username was not specified', async () => {
+                await testReject();
+            });
+
+            it('username was not valid', async () => {
+                await testReject(42);
+            });
+        });
+
+        it('produces correct results', async () => {
+            let resp = await api.nameHistory(UUID);
+            
+            chai.expect(resp).to.be.instanceof(Array);
+        });
+    });
+
+    describe('nameToUuid', () => {
+        describe('throws an error if', () => {
+            async function testReject(...args) {
+                await chai.expect(api.nameToUuid(...args)).be.rejectedWith(Error);
+            }
+
+            it('names are not specified', async () => {
+                await testReject();
+            });
+
+            it('names was not valid', async () => {
+                await testReject(42);
+            });
+        });
+
+        describe('works when names is a', async () => {
+            it('string', async () => {
+                let resp = await api.nameToUuid(USERNAME);
+
+                chai.expect(resp).to.be.instanceof(Array);
+            });
+
+            it('single-element Array', async () => {
+                let resp = await api.nameToUuid([USERNAME]);
+
+                chai.expect(resp).to.be.instanceof(Array);
+            });
+
+            it('multi-element Array', async () => {
+                let resp = await api.nameToUuid([USERNAME, USERNAME2]);
+
+                chai.expect(resp).to.be.instanceof(Array);
+            });
+        });
+    });
+
+    describe('profile', async () => {
+        describe('throws an Error if', async () => {
+            async function testReject(...args) {
+                await chai.expect(api.profile(...args)).be.rejectedWith(Error);
+            }
+
+            it('uuid was not specified', async () => {
+                await testReject();
+            });
+
+            it('uuid was not valid', async () => {
+                await testReject(42);
+            });
+        });
+
+        it('produces correct results', async () => {
+            let resp = await api.profile(UUID);
+            
+            chai.expect(resp).to.have.property('id', UUID);
+            chai.expect(resp).to.have.property('name', USERNAME);
+            chai.expect(resp).to.have.property('properties').and.to.be.instanceof(Array);
+        });
+    });
+});
